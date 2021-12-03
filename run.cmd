ea8f9979553c0e7b421600203f07ab3a72d55f4c
Bram <b.j.h.otte@student.utwente.nl>
2021-12-03 20:12:31 +0100

    aoc day 2

--- run.cmd
+++ run.cmd
@@ -3,3 +3,8 @@ node js/emulator/urcl-emu.js examples/aoc21/1.1.urcl examples/aoc21/1-i.txt
 
 node js/emulator/urcl-emu.js examples/aoc21/1.2.urcl examples/aoc21/1-e.txt
 node js/emulator/urcl-emu.js examples/aoc21/1.2.urcl examples/aoc21/1-i.txt
+
+node js/emulator/urcl-emu.js examples/aoc21/2.1.urcl examples/aoc21/2-e.txt
+node js/emulator/urcl-emu.js examples/aoc21/2.1.urcl examples/aoc21/2-i.txt
+node js/emulator/urcl-emu.js examples/aoc21/2.2.urcl examples/aoc21/2-e.txt
+node js/emulator/urcl-emu.js examples/aoc21/2.2.urcl examples/aoc21/2-i.txt
