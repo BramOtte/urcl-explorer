b208a598918284a6ed3129523fc923336f78b3f1
Bram Otte <bram@otte.biz>
2023-09-25 19:09:47 +0200

    remove build files

--- js/emulator/IEmu.js
+++
@@ -1,8 +0,0 @@
-export var Step_Result;
-(function (Step_Result) {
-    Step_Result[Step_Result["Continue"] = 0] = "Continue";
-    Step_Result[Step_Result["Halt"] = 1] = "Halt";
-    Step_Result[Step_Result["Input"] = 2] = "Input";
-    Step_Result[Step_Result["Debug"] = 3] = "Debug";
-})(Step_Result || (Step_Result = {}));
-//# sourceMappingURL=IEmu.js.map
--- js/emulator/breaks.js
+++
@@ -1,9 +0,0 @@
-export var Break;
-(function (Break) {
-    Break[Break["ONREAD"] = 1] = "ONREAD";
-    Break[Break["ONWRITE"] = 2] = "ONWRITE";
-})(Break || (Break = {}));
-export function break_flag(flags) {
-    return flags.reduce((a, b) => a | b, 0);
-}
-//# sourceMappingURL=breaks.js.map
