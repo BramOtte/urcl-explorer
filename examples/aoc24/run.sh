977ece313c9fdbbfb43356973193f74055d3800c
Bram Otte <bram@otte.biz>
2024-12-02 10:38:13 +0100

    aoc24 day2 part 1

--- examples/aoc24/run.sh
+++ examples/aoc24/run.sh
@@ -2,3 +2,6 @@ set -e
 
 urcx-emu ${0%/*}/1.urcl --text-file ${0%/*}/1-e.txt --wasm
 urcx-emu ${0%/*}/1.urcl --text-file ${0%/*}/1-i.txt --wasm
+
+urcx-emu ${0%/*}/2.urcl --text-file ${0%/*}/2-e.txt --wasm
+urcx-emu ${0%/*}/2.urcl --text-file ${0%/*}/2-i.txt --wasm
